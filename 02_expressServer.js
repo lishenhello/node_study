a0a8334ed9ebee74adabeb96028218a879dfadf8
aa <aa@sina.com>
2018-09-11 21:43:06 +0800

    Signed-off-by: aa <aa@sina.com>

---
+++ 02_expressServer.js
@@ -0,0 +1,18 @@
+// 使用express搭建一个简单的node服务。
+// 访问localhost：1000时输出‘hello express!’
+
+// 引入express框架 颜色chalk插件
+const app = require('express')();
+const chalk = require('chalk');
+
+// 发送请求并响应
+// 这里不能省略req。。否则报错
+app.get('/', (req, res) => {
+	res.writeHead('200', {'Content-Type': 'text/plain;charset=utf-8;'});
+	res.end('hello express!');
+});
+
+// 监听端口
+app.listen('1000', () => {
+	console.log(chalk.rgb(255, 26, 28)('thr server is running'));
+});
